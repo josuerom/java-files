e2e2a2aeca0d24982c5316d578f1f75732da26d1
Josué Romero <79048209+josuerom@users.noreply.github.com>
2023-04-02 05:34:10 -0500

    Template java CP

--- Template for programming competitions.java
+++ Template for programming competitions.java
@@ -3,15 +3,23 @@
  * @created  31/03/23 21:29:54
  * @function Plantilla enfocada para competencias de programación -> vjudge.net codeforces.com
 **/
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.StringTokenizer;
+import java.lang.StringBuilder;
 import java.util.Arrays;
-import java.text.DecimalFormat;
 import static java.lang.Math.*;
 
 public class TemplateCP {
+
+   public static void main(String[] args) throws IOException {
+      Scanner sc = new Scanner();
+      PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+      int tt = Integer.parseInt(sc.next());
+      while (tt-- > 0) {
+         //Write solve!
+      }
+      pr.close();
+   }
 
    static class Scanner {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,14 +46,6 @@ public class TemplateCP {
             st = new StringTokenizer(line);
          }
          return true;
-      }
-   }
-
-   public static void main(String[] args) throws IOException {
-      Scanner sc = new Scanner();
-      int tt = Integer.parseInt(sc.next());
-      while (tt-- > 0) {
-         ${1:Cursor_here}
       }
    }
 }
